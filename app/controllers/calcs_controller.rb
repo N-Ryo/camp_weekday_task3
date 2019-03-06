d9da0cd8074efb56d25f9a864e8b6e0b857c4223
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-06 17:28:59 +0900

    modify calc-CV files

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,14 +3,13 @@ class CalcsController < ApplicationController
     #http://localhost:3000/calcs/:num1/:calc/num2
     #showメソッド内でしか使わない変数はローカル変数で定義する。
     #num1,num2は計算処理で扱う、かつ、paramsで渡される型はStringなのでIntegerへ変換する。
-    #Viewへ渡す変数はresultsだけなので、resultsのみインスタンス変数とする。
+    #Viewへ渡す変数はresultだけなので、resultのみインスタンス変数とする。
     num1 = params[:num1].to_i
     num2 = params[:num2].to_i
     calc = params[:calc]
-    #空の配列@resultsを定義
-    @results = []
     #:calcで計算処理を分岐させる。
-    @results << case calc
+    #caseの返り値を@resultに格納する。
+    @result =  case calc
                 when "addition"
                   num1 + num2
                 when "subtraction"
@@ -18,14 +17,14 @@ class CalcsController < ApplicationController
                 when "multiplication"
                   num1 * num2
                 when "division"
+                  #ZeroDivisionErrorを検出する
                   begin
-                    [num1 / num2 , num1 % num2]
-                  rescue
-                    "divided by #{num2}"
+                    "#{num1 / num2}..#{num1 % num2}"
+                  #「rescue => e」でエラーコードを変数eに格納する
+                  rescue => e
+                    e
                   end
                 end
-    #divisionで商と余りの配列が入れ子になっているので、flatten!メソッドで均す。
-    @results.flatten!
   end
 end
 
