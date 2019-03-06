e972108d06487efd32937be9bfe8c496e6a776b1
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-06 15:27:48 +0900

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #支持通り、演算子を挟む形でリクエストURLを宣言する。
   get 'calcs/:num1/:calc/:num2', to: 'calcs#show'
 end
