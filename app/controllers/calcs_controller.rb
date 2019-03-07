f25185bac17df045c2a3b4317f3200dc861f2111
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-07 19:03:59 +0900

    modify CalcsController for refactoring

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,16 +3,19 @@ class CalcsController < ApplicationController
   include Common
   def show
     #http://localhost:3000/calcs/:num1/:calc/num2
+    num1 = params[:num1]
+    num2 = params[:num2]
+    calc = params[:calc]
     #:calcのURL判定のための配列calcsを定義
     calcs = %w(addition subtraction multiplication division)
-    #num1,num2が半角英数字の羅列であることを判定
-    @result = if params[:num1] =~ /[^0-9]/ || params[:num2] =~ /[^0-9]/
+    #num1,num2が半角数字の羅列であることを判定
+    @result = if num1 =~ /[^0-9]/ || num2 =~ /[^0-9]/
                 "numエラーが発生しました。半角英数字で入力してください。"
               else
                 #:calcの判定
-                if calcs.include?(params[:calc])
+                if calcs.include?(calc)
                   #calcモジュールの引数は3つ
-                  calculation(params[:num1].to_i, params[:calc], params[:num2].to_i)
+                  calculation(num1.to_i, calc, num2.to_i)
                 else
                   #joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
                   "calcエラーが発生しました。正しいURL [ #{calcs.join(", ")} ]"
