16305d34ad4a92704a256e129e24500bfdb71efe
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-07 12:42:11 +0900

    fix wrong url error

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -7,17 +7,30 @@ class CalcsController < ApplicationController
     num1 = params[:num1].to_i
     num2 = params[:num2].to_i
     calc = params[:calc]
-    #:calcで計算処理を分岐させる
-    @result = case calc
-              when "addition"
-                num1 + num2
-              when "subtraction"
-                num1 - num2
-              when "multiplication"
-                num1 * num2
-              when "division"
-                #0で割る場合、エラーになるので、三項演算子でエラーメッセージも表示できるようにする。
-                num2 == 0 ? "0で割ることはできません" : num1 / num2
+    #:calcのURL判定のための配列calcsを定義
+    calcs = %w(addition subtraction multiplication division)
+
+    #num1,num2が半角英数字の羅列であることを判定
+    @result = if params[:num1] =~ /[^0-9]/ || params[:num2] =~ /[^0-9]/
+                "numエラーが発生しました。半角英数字で入力してください。"
+              else
+                #:calcの判定
+                if calcs.include?(params[:calc])
+                  case calc
+                  when "addition"
+                    num1 + num2
+                  when "subtraction"
+                    num1 - num2
+                  when "multiplication"
+                    num1 * num2
+                  when "division"
+                    #0で割る場合、エラーになるので、三項演算子でエラーメッセージも表示できるようにする。
+                    num2 == 0 ? "0で割ることはできません" : num1 / num2
+                  end
+                else
+                  #joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
+                  "calcエラーが発生しました。正しいURL [ #{calcs.join(", ")} ]"
+                end
               end
   end
 end
