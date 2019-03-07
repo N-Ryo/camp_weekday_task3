182a2edcb19f481be0c1ab4c83a123c0ffcc480f
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-07 12:38:43 +0900

    fix wrong url error

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,14 +3,20 @@ class CalcsController < ApplicationController
   include Common
   def show
     #http://localhost:3000/calcs/:num1/:calc/num2
-    #calcモジュールの引数は3つ
-    calc(params[:num1].to_i, params[:calc], params[:num2].to_i)
+    #:calcのURL判定のための配列calcsを定義
+    calcs = %w(addition subtraction multiplication division)
+    #num1,num2が半角英数字の羅列であることを判定
+    @result = if params[:num1] =~ /[^0-9]/ || params[:num2] =~ /[^0-9]/
+                "numエラーが発生しました。半角英数字で入力してください。"
+              else
+                #:calcの判定
+                if calcs.include?(params[:calc])
+                  #calcモジュールの引数は3つ
+                  calculation(params[:num1].to_i, params[:calc], params[:num2].to_i)
+                else
+                  #joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
+                  "calcエラーが発生しました。正しいURL [ #{calcs.join(", ")} ]"
+                end
+              end
   end
 end
-
-
-
-
-
-
-
--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -2,10 +2,10 @@ module Common
   extend ActiveSupport::Concern
   #CalcControllerで使用する計算処理をmoduleで管理する。
   #paramsの値は引数として渡す。
-  def calc(num1, calc, num2)
+  def calculation(num1, calc, num2)
     #:calcで計算処理を分岐させる。
     #caseの返り値を@resultに格納する。
-    @result = case calc
+    case calc
               when "addition"
                 num1 + num2
               when "subtraction"
@@ -15,11 +15,7 @@ module Common
               when "division"
                 #ZeroDivisionErrorを検出する
                 begin
-                  if num1 % num2 == 0
-                    num1 / num2
-                  else
-                    "#{num1 / num2}..#{num1 % num2}"
-                  end
+                  "#{num1 / num2}..#{num1 % num2}"
                 #「rescue => e」でエラーコードを変数eに格納する
                 rescue => e
                   e
