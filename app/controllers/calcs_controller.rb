a33fb49c7405b86c9848ccf3d9ec541490349eb1
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-13 18:07:41 +0900

    modify Em, Half size response

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,21 +1,20 @@
 class CalcsController < ApplicationController
   def show
-    #http://localhost:3000/calcs/:num1/:calc/num2
+    #http://localhost:3000/calcs/:num1/:calc/:num2
     #showメソッド内でしか使わない変数はローカル変数で定義する。
     #num1,num2は計算処理で扱う、かつ、paramsで渡される型はStringなのでIntegerへ変換する。
     #Viewへ渡す変数はresultだけなので、resultのみインスタンス変数とする。
-    num1 = params[:num1].to_i
-    num2 = params[:num2].to_i
+    num1 = params[:num1].tr("０-９", "0-9").to_i
+    num2 = params[:num2].tr("０-９", "0-9").to_i
     calc = params[:calc]
     #:calcのURL判定のための配列calcsを定義
     calcs = %w(addition subtraction multiplication division)
 
     #num1,num2が半角英数字の羅列であることを判定
-    @result = if params[:num1] =~ /[^0-9]/ || params[:num2] =~ /[^0-9]/
-                "numエラーが発生しました。半角英数字で入力してください。"
-              else
+    @result = if params[:num1] =~ /^(-[0-9０-９]*)?([0-9０-９]+.[0-9０-９]*)?([0-9０-９]+)$/ &&
+                   params[:num2] =~ /^(-[0-9０-９]*)?([0-9０-９]+.[0-9０-９]*)?([0-9０-９]+)$/
                 #:calcの判定
-                if calcs.include?(params[:calc])
+                if calcs.include?(calc)
                   case calc
                   when "addition"
                     num1 + num2
@@ -31,6 +30,8 @@ class CalcsController < ApplicationController
                   #joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
                   "calcエラーが発生しました。正しいURL [ #{calcs.join(", ")} ]"
                 end
+              else
+                "numエラーが発生しました。半角英数字で入力してください。"
               end
   end
 end
