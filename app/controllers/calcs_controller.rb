306cca84872bcadc16c5a3af6b63a74bd44ab9f5
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-15 11:08:18 +0900

    Modify and Add comments

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,19 +1,20 @@
 class CalcsController < ApplicationController
   def show
-    #http://localhost:3000/calcs/:num1/:calc/:num2
-    #showメソッド内でしか使わない変数はローカル変数で定義する。
-    #num1,num2は計算処理で扱う、かつ、paramsで渡される型はStringなのでIntegerへ変換する。
-    #Viewへ渡す変数はresultだけなので、resultのみインスタンス変数とする。
+    # http://localhost:3000/calcs/:num1/:calc/:num2
+    # showメソッド内でしか使わない変数はローカル変数で定義する。
+    # num1, num2が全角数字の場合、半角数字に矯正するためにtrメソッドを使用する。
+    # num1, num2は計算処理で扱う、かつ、paramsで渡される型はStringなのでIntegerへ変換する。
+    # Viewへ渡す変数はresultだけなので、resultのみインスタンス変数とする。
     num1 = params[:num1].tr("０-９", "0-9").to_i
     num2 = params[:num2].tr("０-９", "0-9").to_i
     calc = params[:calc]
-    #:calcのURL判定のための配列calcsを定義
+    # :calcのURL判定のための配列calcsを定義
     calcs = %w(addition subtraction multiplication division)
 
-    #num1,num2が半角英数字の羅列であることを判定
+    # num1,num2が数値であることを判定
     @result = if params[:num1] =~ /^(-[0-9０-９]*)?([0-9０-９]+.[0-9０-９]*)?([0-9０-９]+)$/ &&
                    params[:num2] =~ /^(-[0-9０-９]*)?([0-9０-９]+.[0-9０-９]*)?([0-9０-９]+)$/
-                #:calcの判定
+                # :calcの判定
                 if calcs.include?(calc)
                   case calc
                   when "addition"
@@ -23,15 +24,15 @@ class CalcsController < ApplicationController
                   when "multiplication"
                     num1 * num2
                   when "division"
-                    #0で割る場合、エラーになるので、三項演算子でエラーメッセージも表示できるようにする。
+                    # 0で割る場合、エラーになるので、三項演算子でエラーメッセージも表示できるようにする。
                     num2 == 0 ? "0で割ることはできません" : num1 / num2
                   end
                 else
-                  #joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
+                  # joinメソッドで配列calcsを「, 」で区切ってString型に変更して出力
                   "calcエラーが発生しました。正しいURL [ #{calcs.join(", ")} ]"
                 end
               else
-                "numエラーが発生しました。半角英数字で入力してください。"
+                "numエラーが発生しました。数値を入力してください。"
               end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #支持通り、演算子を挟む形でリクエストURLを宣言する。
+  # 支持通り、演算子を挟む形でリクエストURLを宣言する。
   get 'calcs/:num1/:calc/:num2', to: 'calcs#show', constraints: { num1: /[^\/]+/, num2: /[^\/]+/ }
 end
