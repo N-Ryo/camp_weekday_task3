36534f3a449d54c1a8fce634eeeba2cb90fc9c1e
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-15 11:20:41 +0900

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # 支持通り、演算子を挟む形でリクエストURLを宣言する。
+  # num1, num2に関して、小数点を含むパラメタを拡張子と判定させないためにconstraintsオプションを追加
   get 'calcs/:num1/:calc/:num2', to: 'calcs#show', constraints: { num1: /[^\/]+/, num2: /[^\/]+/ }
 end
