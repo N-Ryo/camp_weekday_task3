432c6a5856a448beb2e2e19c301fbd87d57a9003
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-15 11:26:25 +0900

    Add period response

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #支持通り、演算子を挟む形でリクエストURLを宣言する。
-  get 'calcs/:num1/:calc/:num2', to: 'calcs#show'
+  # 支持通り、演算子を挟む形でリクエストURLを宣言する。
+  # num1, num2に関して、小数点を含むパラメタを拡張子と判定させないためにconstraintsオプションを追加
+  get 'calcs/:num1/:calc/:num2', to: 'calcs#show', constraints: { num1: /[^\/]+/, num2: /[^\/]+/ }
 end
